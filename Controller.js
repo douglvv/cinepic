160168189543c69adb496b170ca4111a196e3d5c
Douglas Vidal <vidaldl01@gmail.com>
2023-04-11 11:55:54 -0300

    Update

--- Controller.js
+++ Controller.js
@@ -1,21 +1,17 @@
 const axios = require('axios');
 const { query } = require('express');
+const key = require('./key')
 
 const Controller = {
     async searchMovie(req, res) {
         try {
-            let query = ''
-            console.log(req.query.movie1)
-            if (req.query.movie1) {
-                query = req.query.movie1.trim();
-            }
-            
-            const response = await axios.get(`http://www.omdbapi.com/?apikey=6b526007&s=${query}`);            
+            let query = req.query.query;
+            const response = await axios.get(`http://www.omdbapi.com/?apikey=${key}&s=${query}`);            
             const movies = response.data.Search;
+            console.log(movies)
             res.json(movies)
         } catch (error) {
             console.log(error);
-            res.render('error');
         }
     }
 };
