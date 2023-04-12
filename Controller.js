1c1d87e77ec284cd734e12a439730a39e4aaf504
Douglas Vidal <vidaldl01@gmail.com>
2023-04-12 17:48:29 -0300

    Update 12/04
    - Tratamento de erros no controller;
    - Formatação do código;
    - Alterei alguns nomes de variáveis.

--- Controller.js
+++ Controller.js
@@ -6,46 +6,56 @@ const Controller = {
     async searchMovie(req, res) {
         try {
             const key = req.key;
-            let query = req.query.query;
-                // console.log('Key: '+ key + '\t Query: '+query)
-            
-            // Check if the query is empty
-            // if(!query){
-            //     let errorMsg = true
-            //    res.render('home', {errorMsg: errorMsg})
-            // } else{
-                // Fetches the data from de API and send the data to the view
-                const response = await axios.get(`http://www.omdbapi.com/?apikey=${key}&s=${query}`);            
-                const movies = await response.data.Search
-                // console.log(movies)
-                res.render('home', {movies: movies});
-            // }
+            let query = req.query.title;
+            console.log("Key: " + key + "\tQuery: " + query)
+
+            // Fetches the data from de API
+            const response = await axios.get(`http://www.omdbapi.com/?apikey=${key}&s=${query}`);
+            const result = await response.data.Search
+
+            // Checks if there are results for the search
+            if (response.data.Response == "True") { // Sends the data to the view
+                console.log("Response: " + response.data.Response)
+                console.log(result)
+                res.render('home', { result: result });
+            } else { // Sends the error message to the view
+                let errorMsg = "No results found, please try searching again."
+                console.log("errorMsg: " + errorMsg)
+                res.render('home', { errorMsg: errorMsg })
+            }
         } catch (error) {
             console.log(error);
         }
     },
 
     async getMovie(req, res) {
-        try{
-           const key = req.key;
-           const type = req.params.type;
-           const imdbId = req.params.id;
+        try {
+            const key = req.key;
+            const type = req.params.type;
+            const imdbId = req.params.id;
+                console.log("Key: "+ key + "\tType: " + type + "\timdbId: " + imdbId)
 
-           const response = await axios.get(`http://www.omdbapi.com/?apikey=${key}&type=${type}&i=${imdbId}`);
-           const data = await response.data;
+            // Fetches the data from de API
+            const response = await axios.get(`http://www.omdbapi.com/?apikey=${key}&type=${type}&i=${imdbId}&plot=full`);
+            const result = await response.data;
+
+            //Check if there is a result for the search
+            if (response.data.Response == "True"){ // Sends the data to the view
+                console.log("Response: " + response.data.Response)
+                console.log(result)
+                res.render('movie/movie', { result: result })    
+            } else { // Sends the error message to the view
+                let errorMsg = true
+                console.log()
+            }
             
-           console.log(data)
-           // Check if there is a result
-        //    if (response.Response == 'True'){
-            res.render('movie/movie', {data: data})
-        //    } else {
-        //     res.redirect('home');
-        //    }
+            
 
-        } catch(error){
+
+        } catch (error) {
             console.log(error)
         }
-        
+
     }
 };
 
